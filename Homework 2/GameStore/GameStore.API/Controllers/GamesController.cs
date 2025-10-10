17439d0548786f8821370063c16b852f7dba1630
Marcosax22 <marcosarielencarnacion@gmail.com>
2025-10-09 20:04:21 -0400

    Adding Homework 2

---
+++ Homework 2/GameStore/GameStore.API/Controllers/GamesController.cs
@@ -0,0 +1,20 @@
+﻿using GameStore.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly GameStoreDbContext _context;
+        public GamesController(GameStoreDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
